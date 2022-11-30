8b87996a70ae1f7ef09103ee59991c77805660a1
Gustavo Vasquez <gustavovasquez@lyft.com>
2022-11-30 03:31:55 -0600

    Submit posts and save them

---
+++ public/js/common.js
@@ -0,0 +1,72 @@
+$("#postTextarea").keyup((event) => {
+    var textbox = $(event.target);
+    var value = textbox.val().trim();
+    
+    var submitButton = $("#submitPostButton");
+
+    if(submitButton.length == 0) return alert("No button found");
+
+    if(value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+
+    submitButton.prop("disabled", false);
+})
+
+$("#submitPostButton").click((event) => {
+    var button = $(event.target);
+    var textbox = $("#postTextarea");
+
+    var data = {
+        content: textbox.val()
+    }
+
+    $.post("/api/posts", data, (postData) => {
+        
+        var html = createPostHtml(postData);
+        $(".postsContainer").prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
+    })
+})
+
+function createPostHtml(postData) {
+    
+    var postedBy = postData.postedBy;
+    var displayName = postedBy.firstName + " " + postedBy.lastName;
+    var timestamp = postData.createdAt;
+
+    return `<div class='post'>
+
+                <div class='mainContentContainer'>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <span class='displayName'>${displayName}</span>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${timestamp}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-comment'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-retweet'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-heart'></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
